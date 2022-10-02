9662ea1f962f3e07301e181b89839acb97b02c19
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-02 21:42:39 +0700

    start implement Address class

---
+++ Simulator/Address.java
@@ -0,0 +1,18 @@
+package Simulator;
+
+public class Address {
+    protected int value;
+
+    public Address(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+}
