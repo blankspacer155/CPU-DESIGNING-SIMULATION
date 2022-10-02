e4940c2e28ff9690694f4da40250e425630c9439
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-02 21:44:27 +0700

    [not finish] start implement Register.java

---
+++ Simulator/Registers.java
@@ -0,0 +1,9 @@
+package Simulator;
+
+public class Registers {
+    protected int[] registers;
+    protected int regA;
+    protected int regB;
+    protected int wrReg;
+    protected int wrData;
+}
