0b1d9de86952e43da04509cf9517aae8afa5050d
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-02 21:43:15 +0700

    start implement Memory.java

---
+++ Simulator/Memory.java
@@ -0,0 +1,61 @@
+package Simulator;
+
+public class Memory {
+    private int[] memory;
+    private Address instrAddr;
+    private Address memAddr;
+    private int MemWrite;
+    private int MemRead;
+
+    public Memory(int[] memory) {
+        this.memory = memory;
+        this.instrAddr = new Address(0);
+        this.memAddr = new Address(0);
+        MemWrite = 0;
+        MemRead = 0;
+    }
+
+    public void setInstrAddr(int PC) {
+        this.instrAddr.setValue(PC);
+    }
+
+    public void setMemAddr(int memAddr) {
+        this.memAddr.setValue(memAddr);
+    }
+
+    public void setCtrlWrite(int ctrl){
+        this.MemWrite = ctrl;
+    }
+
+    public void setCtrlRead(int ctrl){
+        this.MemRead = ctrl;
+    }
+
+    public void writeData(int wrData){
+        assert MemWrite == 1;
+        memory[memAddr.getValue()] = wrData;
+    }
+
+    public int getInstruction(){
+        return memory[instrAddr.getValue()];
+    }
+
+    public int readData(){
+        assert MemRead == 1;
+        return memory[memAddr.getValue()];
+    }
+
+    
+
+    
+    
+   
+
+    
+    
+
+    
+
+    
+
+}
