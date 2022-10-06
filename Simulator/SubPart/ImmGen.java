187fa3f6ff275490121b36fdd6509dcac709d3a7
Nuttapong9911 <nuttapongboonsala@gmail.com>
2022-10-06 11:03:05 +0700

    fix 2's complement  in Immgen

--- Simulator/SubPart/ImmGen.java
+++ Simulator/SubPart/ImmGen.java
@@ -11,32 +11,35 @@ public class ImmGen {
     // --------------------------------------------
     protected int output;
     // --------------------------------------------
-
-    // gate for 2's complement
-    // --------------------------------------------
-    protected Gate gate;
-    // --------------------------------------------
-
     
     public ImmGen() {
         input = 0;
         output = 0;
-        gate = new Gate();
     }
 
 
     public void excute(){
         if (((input >> 15) & 1) == 1){
             input &= 0b1111111111111111;
-            gate.setInput(input, input);
-            gate.NAND();
-            input &= 0b1111111111111111;
-            output = -(gate.getResult() + 1);
+            output = input & input;
+
+            // 16 bits nand
+            for(int i = 0; i <= 15; i ++){
+                if(((output >> (i)) & 1) == 1){
+                    output = output - (1 << i);
+                }else{
+                    output = output + (1 << i);
+                } 
+            }
+
+            output &= 0b1111111111111111;
+            output = -(output + 1);
         }else
             output =  input & 0b1111111111111111;
     }
 
     public void setInput(int input) {
+        output = 0;
         this.input = input;
     }
 
