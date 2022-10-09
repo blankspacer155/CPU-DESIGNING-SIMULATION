412163cbf02abcc197277f0af909df9bbc7bc64b
Thanakun <Thanakun_Ch@cmu.ac.th>
2022-10-09 23:51:33 +0700

    update Multiplication

--- Program_Assembly/Multiplication.cpp
+++ Program_Assembly/Multiplication.cpp
@@ -31,21 +31,24 @@ int main(){
 
 
 /*Aseembly Multiplication
-"              lw 0 2 sign $2 = 1073741824  \n" + 
+ "      lw   0 3 one iterator = 1\n" +
+        "      lw   0 2 sign $2 = 1073741824  \n" + 
         "      lw   0 6 mcand   $6 = mcand  \n" + 
-        "      lw   0 7 mplier $7 = mplier  \n" + 
-        "mul   nand 6 3  5 (plier nand iterator) = temp \n" + 
+        "      lw   0 7 mplier $7 = mplier  \n" +
+        "mul   nand 7  3  5 (plier nand iterator) = temp \n" + 
         "      nand 5 5  5  (temp nand temp) = temp \n" + 
-        "      beq  5 0 l1  if(temp == zero) go to L1 \n" + 
-        "      add  1 1  1  result = result + result\n" + 
-        "l1    add  3 3  3  iterator = iterator + iterator \n"+
-        "      add  7 7  7    cand = cand + cand  \n" + 
-        "      beq  2 3  end   if(iterator == sign) go to exit \n"+
+        "      beq  0 5 2  if(temp == zero) go to L1 \n" + 
+        "      add  1 6 1  result = result + cand\n" + 
+        "      add  3 3  3  iterator = iterator + iterator \n"+
+        "      add  6 6  6    cand = cand + cand  \n" + 
+        "      beq  2 3  2   if(iterator == sign) go to exit \n"+
         "      beq  0 0  mul   jump to multiplication \n" + 
-        "end   halt  \n"+
+        "      noop          \n" +
+        "      halt  \n"+
+        "one .fill 1 \n"+
         "mcand  .fill 32766 \n" + 
-        "mplier .fill 10838\n" +  
-        "sign .fill 1073741824 "  ;                     
+        "mplier .fill 10383\n" +  
+        "sign .fill 1073741824 " ;                    
  */
 
 
