d59e3371f0e5f987c8a940a317b4e47aae802f4b
earth <konthakarn_f@cmu.ac.th>
2022-10-16 23:15:58 +0700

    pa

--- ProgramTest.java
+++ ProgramTest.java
@@ -17,59 +17,19 @@ public class ProgramTest {
         
         
         String src=
-        "      lw   0 1 n     $1 = 3  \n" +
-        "      lw   0 2 r     $2 = 2  \n" +
-        "      lw   0 4 com   save $4 = combination \n" +
-        "      jalr 4 7       call com ; $7 = return address \n" +  
-        "comb  beq  2 0 dog      if (r == 0 ) call c \n" + 
-        "      lw   0 6 pos1   $6 = 1  \n" + 
-        "      sw   5 1 stack  stored $1 in stack \n" + 
-        "      add  5 6 5      increment stack pointer \n "+
-        "      sw   5 2 stack  stored $2 in stack \n" +
-        "      add  5 6 5      increment stack pointer \n" +
-        "      beq  1 2 coal    if ( n == r ) call cc \n" + 
-        "      lw   0 6 neg1   $6 = -1 \n" +
-        "      add  1 6 1      $1 = $1 -1  (n=n-1) \n" +
-        "      lw   0 6 conti \n" + 
-        "      sw   5 6 stack \n" + 
-        "      lw   0 6 pos1 \n " +
-        "      add  5 6 5    \n " +
-        "      beq  0 0 comb     jump to comb \n" +
-        "coat  lw   0 6 neg1   \n" + 
-        "      add  2 6 2      \n" +
-        "      lw   0 6 pos1   \n" +
-        "      add  3 6 3      \n" +
-        "      add  5 6 5      \n" +
-        "      lw   0 6 tree  \n" +
-        "      sw   5 6 stack  \n" +
-        "      lw   0 6 pos1   \n" +
-        "      add  5 6 5      \n" +
-        "      beq  0 0 comb    \n" + 
-        "third lw   0 6 pos1   \n" +
-        "      add  3 6 3      \n" +
-        "coco  lw   0 6 neg1   \n" +
-        "      add  5 6 5        \n" +
-        "      lw   5 4 stack    load bef to $4 \n" +
-        "      add  5 6 5        stack - 1 \n" + 
-        "      lw   5 2 stack    \n" + 
-        "      add  5 6 5        \n" + 
-        "      lw   5 1 stack    \n" +
-        "      jalr 4 7          \n" +
-        "cat   lw   0 6 pos1     \n" + 
-        "      add  3 6 3        \n" +
-        "      lw   0 6 neg1     \n" + 
-        "      add  5 6 5        \n" + 
-        "      lw   5 4 stack    \n" +
-        "com .fill comb \n" +
-        "n   .fill 3 \n" +
-        "r   .fill 2 \n"+
-        "neg1 .fill -1 \n" + 
-        "pos1 .fill  1 \n" + 
-        "coal .fill coco \n" + 
-        "conti  .fill coat \n"+ 
-        "tree .fill third \n"+ 
-        "dog  .fill cat   \n" +
-        "stack .fill 0 \n";
+        "       lw 0 6 base \n" +
+        "       lw 0 7 exponent \n" +
+        "       lw 0 2 sign \n" +
+        "       lw 0 0 neg1 \n" +
+        "       lw 0 4 one  \n" +
+        "done   halt  \n"+
+
+
+        "one1 .fill 1 \n"+
+        "base .fill 4 \n"+
+        "exponent .fill 3 \n"+
+        "neg1 .fill -1 \n" +
+        "sign .fill 1073741824 \n" ; 
 
         int[] Memory = new int[100];
         int numSrcCode = src.split("\n").length;
@@ -78,12 +38,12 @@ public class ProgramTest {
         Parser p = new Parser(src, bindings,Memory);
         p.evauateAll();
 
-        // System.out.println( p.prettyPrintAll());
+         System.out.println( p.prettyPrintAll());
 
-        // int i=0;
+       // int i=0;
         // for(int ins:Memory){
         //     System.out.println("addr["+(i++)+"] :"+ins);
-        // }
+        //}
 
         Simulator sim = new Simulator();
         sim.setMachineCode(Memory, numSrcCode);
