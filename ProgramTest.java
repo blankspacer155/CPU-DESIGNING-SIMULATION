5e2d13fd001f9f2805b248f3fd31bd3779658d33
earth <konthakarn_f@cmu.ac.th>
2022-10-17 13:41:44 +0700

    expo comple

--- ProgramTest.java
+++ ProgramTest.java
@@ -17,26 +17,45 @@ public class ProgramTest {
         
         
         String src=
-        "      lw   0 3 one iterator = 1\n" +
-        "      lw   0 2 sign $2 = 1073741824  \n" + 
-        "      lw   0 6 mcand   $6 = mcand  \n" + 
-        "      lw   0 7 mplier $7 = mplier  \n" +
-        "mul   nand 7  3  5 (plier nand iterator) = temp \n" + 
-        "      nand 5 5  5  (temp nand temp) = temp \n" + 
-        "      beq  0 5 cat  if(temp == zero) go to L1 \n" + 
-        "      add  1 6 1  result = result + cand\n" + 
-        "cat     add  3 3  3  iterator = iterator + iterator \n"+
-        "      add  6 6  6    cand = cand + cand  \n" + 
-        "      beq  2 3  done   if(iterator == sign) go to exit \n"+
-        "      beq  0 0  mul   jump to multiplication \n" + 
-        "      noop          \n" +
-        "done  halt end of program  \n"+
+        "       lw 0 6 base \n" +
+        "       lw 0 7 exponent \n" +
+        "       lw 0 2 sign \n" +
+        "       lw 0 4 one temp1 = 1 \n" +
+        "       beq 0 7 dog if(exponent == 0) base ^ 0 \n" +
+        "       beq 7 4 cat if(exponent == 1) base ^ 1 \n" +
+        "       lw 0 5 base temp2 = base \n" +
+        "expo   beq 7 4 done if(exponent == 1) end program \n" +
+        "       lw 0 4 neg1 temp1 = -1 \n" +
+        "       add 7 4 7 exponent = exponent - 1 \n" +
+        "       lw 0 4 zero \n" +
+        "       add 5 0 4 \n" +
+        "       lw 0 1 zero \n" +
+        "       lw   0 3 one iterator = 1 \n" +
+        "mul    nand 4  3  5 (plier nand iterator) = temp2 \n" + 
+        "       nand 5 5  5  (temp nand temp) = temp2 \n" + 
+        "       beq  0 5 tree  if(temp == zero) go to L1 \n" + 
+        "       add  1 6 1  result = result + cand\n" + 
+        "tree   add  3 3  3  iterator = iterator + iterator \n"+
+        "       add  6 6  6    cand = cand + cand  \n" + 
+        "       beq  2 3  to   if(iterator == sign) go to exit \n"+
+        "       beq  0 0  mul   jump to multiplication \n" +
+        "to     lw 0 5 zero \n" +
+        "       add 1 0 5  ******\n" + 
+        "       lw 0 4 one temp1 = 1 \n" +
+        "       lw   0 6 base   reset base  \n" + 
+        "       beq 0 0 expo jump to Exponention \n" +
+        "cat    lw 0 1 base answer = base \n" +
+        "       beq 0 0 done \n" +
+        "dog  lw 0 1 one set answer = 1 \n" +
+        "done   halt  \n"+
+        "base .fill 3 \n"+
+        "exponent .fill 4 \n"+
+        "zero .fill 0 \n" +
         "one .fill 1 \n"+
-        "mcand  .fill 32766 \n" + 
-        "mplier .fill 10383\n" +  
-        "sign .fill 1073741824 " ;
+        "neg1 .fill -1 \n" +
+        "sign .fill 1073741824 " ;  
 
-        int[] Memory = new int[500];
+        int[] Memory = new int[700];
         int numSrcCode = src.split("\n").length;
 
 
--- Program_Assembly/Exponential.cpp
+++ Program_Assembly/Exponential.cpp
@@ -9,30 +9,33 @@
         "expo   beq 7 4 done if(exponent == 1) end program \n" +
         "       lw 0 4 neg1 temp1 = -1 \n" +
         "       add 7 4 7 exponent = exponent - 1 \n" +
-        "       lw 0 4 one temp1 = 1 \n" +
+        "       lw 0 4 zero \n" +
+        "       add 5 0 4 \n" +
+        "       lw 0 1 zero \n" +
         "       lw   0 3 one iterator = 1 \n" +
-        "       lw   0 6 base   reset base  \n" + 
-        "mul    nand 5  3  5 (plier nand iterator) = temp2 \n" + 
+        "mul    nand 4  3  5 (plier nand iterator) = temp2 \n" + 
         "       nand 5 5  5  (temp nand temp) = temp2 \n" + 
-        "       beq  0 5 2  if(temp == zero) go to L1 \n" + 
+        "       beq  0 5 tree  if(temp == zero) go to L1 \n" + 
         "       add  1 6 1  result = result + cand\n" + 
-        "       add  3 3  3  iterator = iterator + iterator \n"+
+        "tree   add  3 3  3  iterator = iterator + iterator \n"+
         "       add  6 6  6    cand = cand + cand  \n" + 
-        "       beq  2 3  2   if(iterator == sign) go to exit \n"+
+        "       beq  2 3  to   if(iterator == sign) go to exit \n"+
         "       beq  0 0  mul   jump to multiplication \n" +
-        "       lw 0 5 zero \n" +
-        "       add 5 0 1 \n" + 
+        "to     lw 0 5 zero \n" +
+        "       add 1 0 5  ******\n" + 
+        "       lw 0 4 one temp1 = 1 \n" +
+        "       lw   0 6 base   reset base  \n" + 
         "       beq 0 0 expo jump to Exponention \n" +
         "cat    lw 0 1 base answer = base \n" +
         "       beq 0 0 done \n" +
         "dog  lw 0 1 one set answer = 1 \n" +
         "done   halt  \n"+
-        "base .fill 4 \n"+
-        "exponent .fill 3 \n"+
+        "base .fill 3 \n"+
+        "exponent .fill 4 \n"+
         "zero .fill 0 \n" +
         "one .fill 1 \n"+
         "neg1 .fill -1 \n" +
-        "sign .fill 1073741824 \n" ;                    
+        "sign .fill 1073741824 " ;                   
  */
 
 
